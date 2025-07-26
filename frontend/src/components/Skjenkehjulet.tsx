08d29481a93da1b6bfbda0faecc821f5b8b0a513
akselna <124269604+akselna@users.noreply.github.com>
2025-07-26 02:37:20 +0200

    Import Skjenkehjulet styles

--- frontend/src/components/Skjenkehjulet.tsx
+++ frontend/src/components/Skjenkehjulet.tsx
@@ -7,6 +7,7 @@
 
 import React, { useState, useEffect, useRef } from "react";
 import { CustomSocket } from "../types/socket.types";
+import "../styles/Skjenkehjulet.css";
 
 interface SkjenkehjuletProps {
   sessionId: string;
